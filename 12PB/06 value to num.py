c04de1dbc667099bf1faf29d24af01d459788ae1
solrun2 <solrun555@gmail.com>
2022-09-20 23:06:58 +0700

    solrun2

--- 12PB/06 value to num.py
+++ 12PB/06 value to num.py
@@ -2,40 +2,60 @@ money = input().strip()
 if "".join("".join(money.split(".")).split(",")).isdigit() :
     if "," in money and "." in money :
         dotCheck = money.split(".")
-        if len(dotCheck[1]) > 2 :
+        if len(dotCheck[1]) > 2 or money[-1] == "." or money[-1] == "," :
             print("ERROR")
         elif len(dotCheck[1]) > 2 :
             print("ERROR")
         else :
             commaCheck = dotCheck[0].split(",")
             cnt = 0
-            for i in commaCheck :
-                if len(i) > 3 :
+            for i in commaCheck[1::] :
+                if len(i) != 3 :
                     cnt = -1
                     print("ERROR")
                     break
             if cnt == 0 :
+                if len(commaCheck[0]) > 3 :
+                    cnt = -1
+                    print("ERROR")
+            if cnt == 0 :
                 ans = ""
                 ans += "".join(commaCheck) + "." + dotCheck[1]
-                print(float(ans)+1)
+                if len(dotCheck[1]) == 2 :
+                    print("{:.2f}".format(float(ans)+1))
+                else :
+                    print(float(ans)+1)
     elif "," in money :
         commaCheck = money.split(",")
         cnt = 0
         for i in commaCheck :
-            if len(i) > 3 :
+            if len(i) > 3 or money[-1] == "." or money[-1] == ",":
                 cnt = -1
                 print("ERROR")
                 break
+        if cnt == 0 :
+            for i in commaCheck[1::] :
+                if len(i) != 3 :
+                    cnt = -1
+                    print("ERROR")
+                    break
+        if cnt == 0 :
+            if len(commaCheck[0]) > 3 :
+                cnt = -1
+                print("ERROR")
         if cnt == 0 :
             ans = ""
             ans += "".join(commaCheck)
             print(int(ans)+1)
     elif "." in money :
         dotCheck = money.split(".")
-        if len(dotCheck[1]) > 2 :
+        if len(dotCheck[1]) > 2 or money[-1] == "." or money[-1] == "," :
             print("ERROR")
         else :
-            print(float(money)+1)
+            if len(dotCheck[1]) == 2 :
+                print("{:.2f}".format(float(money)+1))
+            else :
+                print(float(money)+1)
     else :
         print(int(money)+1)
 else :
--- 12PB/07 camelCase.py
+++ 12PB/07 camelCase.py
@@ -1,5 +1,5 @@
 s = input().strip()
 for i in "-=_.$" :
-    s = s.replace(i," ")
-s = s.strip().split(" ")
+    s = s.replace(i,".")
+s = s.strip(".").split(".")
 print(s[0].lower() + "".join([i.capitalize() for i in s[1:]]))
