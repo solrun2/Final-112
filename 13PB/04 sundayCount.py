28958ae807ca078e174dcfce07fd47ca1bb9197c
solrun2 <solrun555@gmail.com>
2022-10-02 00:55:49 +0700

    solrun2

--- 13PB/04 sundayCount.py
+++ 13PB/04 sundayCount.py
@@ -1,8 +1,5 @@
-dayOne = input().split("-")
-dayTwo = input().split("-")
+firstDay = input().split("-")
+lastDay = input().split("-")
 firstSunday = int(input())
-if int(dayOne[1]) < 1 or int(dayOne[1]) > 12 or int(dayOne[2]) < 1 or int(dayOne[2]) > 31 or int(dayTwo[1]) < 1 or int(dayTwo[1]) > 12 or int(dayTwo[2]) < 1 or int(dayTwo[2]) > 31 :
-    print("Wrong Month")
-elif firstSunday < 1 or firstSunday > 7 :
-    print("Wrong Day")
+if 
 dayInMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
--- 13PB/test.py
+++ 13PB/test.py
@@ -1,2 +1,26 @@
-s = input()
-print(len(s))
+def extract_string(text) :
+    if text == "" :
+        return []
+    else :
+        ans = []
+        s = ""
+        for i in range(len(text)) :
+            if i+1 < len(text) :
+                if text[i].isnumeric() and not text[i+1].isnumeric() or not text[i].isnumeric() and text[i+1].isnumeric() :
+                    s += text[i]
+                    ans.append(s)
+                    s = ""
+                else :
+                    s += text[i]
+            else :
+                s += text[i]
+                ans.append(s)
+            new_ans = []
+            for i in ans :
+                if i.isnumeric() :
+                    new_ans.append(int(i))
+                else :
+                    new_ans.append(i)
+        return new_ans
+
+print(extract_string(""))
